10765d9fd2763232f33d34745263af16c3d73a33
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-09-02 15:43:32 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
+
+import React from 'react';
 import  './css/main.css';
 import './App.css';
-import React from 'react';
+import ShopItemClass from './components/ShopItemClass';
+
 
 const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-  }
+	brand: 'Tiger of Sweden',
+	title: 'Leonard coat',
+	description: 'Minimalistic coat in cotton-blend',
+	descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+	price: 399,
+	currency: '£'
+	}
 
-class ShopItemClass extends React.Component {
-  render (){
-    return(
-      <div class="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div class="description">
-        {item.descriptionFull}
-      </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{item.currency}{item.price}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-    )
-  }
-}
 function App() {
   return (
     <div className="container">
@@ -40,7 +23,7 @@ function App() {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemClass/>
+        <ShopItemClass item={item}/>
       </div>
     </div>
   );
